90aab95f5cb11ffe67417d301d6317e321bedbc9
minthammock-development <minthammockdevelopment@gmail.com>
2022-05-14 23:13:01 -0600

    finished values.go and created executable.

---
+++ values/values.go
@@ -0,0 +1,10 @@
+package main
+import "fmt"
+func main(){
+	fmt.Println("go" + "lang")
+	fmt.Println("Integer example: 1+1 = ", 1+1)
+	fmt.Println("Float example: 7.0/3.0 = ", 7.0/3.0)
+	fmt.Println("boolean and operator: true and false = ",true && false)
+	fmt.Println("boolean or operator: true or false = ", true || false)
+	fmt.Println("boolean not operator: !true = ",!true)
+}
